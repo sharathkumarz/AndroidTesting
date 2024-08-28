8f878bd9b19101046770a4afb1f171b4c183cb5b
sharathkumarz <sharathkumarenugathala@gmail.com>
2024-08-28 07:43:09 +0530

    testsample

---
+++ app/src/androidTest/java/com/sharathkumar/testing/ExampleInstrumentedTest.kt
@@ -0,0 +1,23 @@
+package com.sharathkumar.testing
+
+import androidx.compose.ui.test.assert
+import androidx.compose.ui.test.assertIsDisplayed
+import androidx.compose.ui.test.junit4.createComposeRule
+import androidx.compose.ui.test.onAllNodesWithText
+import androidx.compose.ui.test.onNodeWithTag
+import androidx.compose.ui.test.onNodeWithText
+import androidx.compose.ui.test.performClick
+import androidx.compose.ui.test.performTextInput
+import androidx.test.platform.app.InstrumentationRegistry
+import androidx.test.ext.junit.runners.AndroidJUnit4
+
+import org.junit.Test
+import org.junit.runner.RunWith
+
+import org.junit.Assert.*
+import org.junit.Rule
+
+@RunWith(AndroidJUnit4::class)
+class AddTwoNumbersTest {
+    
+}
